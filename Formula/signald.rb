f5b0e9b94597bf3e9053e492fa7f2153235a7c2b
Bjørn Magnus Mathisen <bjornmagnus@me.com>
2021-12-09 11:36:59 +0100

    added first formulae

---
+++ Formula/signald.rb
@@ -0,0 +1,18 @@
+class Signald < Formula
+  desc "ANSI C library for parsing GNU-style command-line options"
+  homepage "https://argtable.sourceforge.io"
+  url "https://downloads.sourceforge.net/project/argtable/argtable/argtable-2.13/argtable2-13.tar.gz"
+  version "2.13"
+  sha256 "8f77e8a7ced5301af6e22f47302fdbc3b1ff41f2b83c43c77ae5ca041771ddbf"
+
+  bottle do
+
+  end
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+end
